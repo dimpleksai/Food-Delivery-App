d2e01850614376f39bc9a9925bcd00862944178b
Dimple <dimpleksai@Dimples-MacBook-Air.local>
2025-01-21 22:35:31 -0600

    Add food commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sideBar/SideBar";
@@ -10,6 +12,7 @@ import Orders from "./pages/orders/Orders";
 const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
