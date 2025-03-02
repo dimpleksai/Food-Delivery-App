98814cd4e51608077e9c268fb247ab61d638ff5f
Dimple <dimpleksai@Dimples-MacBook-Air.local>
2025-03-02 02:54:26 -0600

    bug fix

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,9 +1,5 @@
 import express from "express";
-import {
-  placeOrder,
-  userOrders,
-  verifyOrder,
-} from "../controllers/orderController.js";
+import { placeOrder, verifyOrder } from "../controllers/orderController.js";
 import authMiddleWare from "../middleware/auth.js";
 
 const orderRouter = new express.Router();
